183b2d569a3a9baf050ad8b0de33a7107e84f525
Apurba Panja <apurbapanja1@gmail.com>
2025-09-21 14:27:23 +0530

    lint config updates

---
+++ configs/eslint/index.d.ts
@@ -0,0 +1,8 @@
+import type { Linter } from "eslint";
+
+/**
+ * Declares the shape of the default export from @fintrack/eslint-config.
+ * It's an array of ESLint Config objects.
+ */
+declare const config: Linter.Config[];
+export default config;
