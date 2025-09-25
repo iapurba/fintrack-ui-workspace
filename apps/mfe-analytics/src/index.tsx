59a4689b04000cb5986d607661a414fcea5716ee
Apurba Panja <apurbapanja1@gmail.com>
2025-09-26 00:07:26 +0530

    analytics mfe app setup

---
+++ apps/mfe-analytics/src/index.tsx
@@ -0,0 +1,7 @@
+import("./bootstrap").then((module) => {
+  const { mount } = module;
+  const devRoot = document.getElementById("_analytics-dev-root");
+  if (devRoot) {
+    mount(devRoot);
+  }
+});
---
+++ apps/mfe-analytics/webpack.config.ts
@@ -0,0 +1,45 @@
+type Configuration = import("webpack").Configuration;
+const { merge } = require("webpack-merge");
+const {
+  createCommonConfig,
+  createDevConfig,
+  createProdConfig,
+  createModuleFederationConfig,
+} = require("@fintrack/webpack-config");
+const packageJson = require("./package.json");
+
+const PORT = 3003;
+
+const generateWebpackConfig = (
+  _env: unknown,
+  argv: { mode: "development" | "production" },
+): Configuration => {
+  const context = __dirname;
+
+  const federationConfig = createModuleFederationConfig({
+    name: "mfe-user",
+    dependencies: packageJson.dependencies,
+  });
+
+  const commonConfig = createCommonConfig({ context });
+
+  const devConfig = createDevConfig({
+    context,
+    port: PORT,
+    host: "localhost",
+  });
+
+  const prodConfig = createProdConfig({ context });
+
+  if (argv.mode === "development") {
+    return merge(commonConfig, devConfig, {
+      plugins: [federationConfig],
+    });
+  }
+
+  return merge(commonConfig, prodConfig, {
+    plugins: [federationConfig],
+  });
+};
+
+module.exports = generateWebpackConfig;
